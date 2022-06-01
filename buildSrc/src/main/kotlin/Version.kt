932ea9d61b52a17075754bcbda247d03c243578d
Stanley Wintergreen <kepocnhh@gmail.com>
2022-06-01 15:09:53 +0300

    init jacoco

--- buildSrc/src/main/kotlin/Version.kt
+++ buildSrc/src/main/kotlin/Version.kt
@@ -1,4 +1,5 @@
 object Version {
+    const val jacoco = "0.8.8"
     const val jvmTarget = "1.8"
     const val junit = "4.13.2"
     const val kotlin = "1.5.31"
--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -2,6 +2,7 @@ repositories.mavenCentral()
 
 plugins {
     id("org.jetbrains.kotlin.jvm")
+    id("org.gradle.jacoco")
 }
 
 tasks.getByName<JavaCompile>("compileJava") {
@@ -14,4 +15,38 @@ tasks.getByName<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>("compileKotlin"
 
 dependencies {
     testImplementation("junit:junit:${Version.junit}")
+}
+
+tasks.getByName<JavaCompile>("compileTestJava") {
+    targetCompatibility = Version.jvmTarget
+}
+
+tasks.getByName<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>("compileTestKotlin") {
+    kotlinOptions.jvmTarget = Version.jvmTarget
+}
+
+val testTask = tasks.getByName<Test>("test")
+
+jacoco {
+    toolVersion = Version.jacoco
+}
+
+val testCoverageTask = tasks.getByName<JacocoReport>("jacocoTestReport") {
+    dependsOn(testTask)
+    reports {
+        csv.required.set(false)
+        html.required.set(true)
+        xml.required.set(false)
+    }
+}
+
+tasks.getByName<JacocoCoverageVerification>("jacocoTestCoverageVerification") {
+    dependsOn(testCoverageTask)
+    violationRules {
+        rule {
+            limit {
+                minimum = BigDecimal(0.96)
+            }
+        }
+    }
 }
