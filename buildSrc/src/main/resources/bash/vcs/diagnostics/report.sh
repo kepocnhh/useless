54400e21cd817abb4db88c6bd141f0a9017e26b7
Stanley Wintergreen <kepocnhh@gmail.com>
2022-06-03 16:29:27 +0300

    push diagnostics report

--- buildSrc/src/main/resources/bash/vcs/diagnostics/report.sh
+++ buildSrc/src/main/resources/bash/vcs/diagnostics/report.sh
@@ -19,6 +19,36 @@ git -C $REPOSITORY init && \
  git -C $REPOSITORY fetch --depth=1 origin gh-pages && \
  git -C $REPOSITORY checkout gh-pages || exit 1 # todo
 
-exit 1 # todo
+RELATIVE_PATH=$GITHUB_RUN_NUMBER/$GITHUB_RUN_ID/diagnostics/report
+mkdir -p $REPOSITORY/build/$RELATIVE_PATH || exit 1 # todo
+cp -r diagnostics/report/* $REPOSITORY/$RELATIVE_PATH || exit 1 # todo
+
+COMMIT_MESSAGE="CI build #$GITHUB_RUN_NUMBER | $WORKER_NAME added diagnostics report"
+
+TYPES="$(jq -Mcer .types diagnostics/summary.json)" || exit 1 # todo
+if test "$TYPES" == "[]"; then
+ echo "Diagnostics should have determined the cause of the failure!"; exit 1
+fi
+COMMIT_MESSAGE="${COMMIT_MESSAGE} of ${TYPES} issues."
+
+CODE=0
+git -C $REPOSITORY config user.name "$WORKER_NAME" && \
+ git -C $REPOSITORY config user.email "$WORKER_EMAIL"; CODE=$?
+if test $CODE -ne 0; then
+ echo "Git config failed!"; exit 41
+fi
+
+git -C $REPOSITORY add --all . && \
+ git -C $REPOSITORY commit -m "$COMMIT_MESSAGE" && \
+ git -C $REPOSITORY tag "diagnostics/report/$GITHUB_RUN_NUMBER/$GITHUB_RUN_ID"; CODE=$?
+if test $CODE -ne 0; then
+ echo "Git commit failed!"; exit 42
+fi
+
+git -C $REPOSITORY push -f && \
+ git -C $REPOSITORY push --tag; CODE=$?
+if test $CODE -ne 0; then
+ echo "Git push failed!"; exit 43
+fi
 
 exit 0
