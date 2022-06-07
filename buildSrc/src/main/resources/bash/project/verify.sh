f4b6c8d8b01d1fc9a1a29c669586474175b1e337
Stanley Wintergreen <kepocnhh@gmail.com>
2022-06-07 17:13:15 +0300

    fix text coverage

--- buildSrc/src/main/resources/bash/project/verify.sh
+++ buildSrc/src/main/resources/bash/project/verify.sh
@@ -10,7 +10,8 @@ ENVIRONMENT=repository/buildSrc/src/main/resources/json/verify.json
 ARRAY=($(jq -Mcer "keys|.[]" $ENVIRONMENT))
 SIZE=${#ARRAY[*]}
 for ((i=0; i<SIZE; i++)); do
- TASK="$(jq -Mcer ".${ARRAY[i]}.task" $ENVIRONMENT)" || exit 1 # todo
+ TYPE="${ARRAY[i]}"
+ TASK="$(jq -Mcer ".${TYPE}.task" $ENVIRONMENT)" || exit 1 # todo
  gradle -p repository "$TASK"; CODE=$?
  if test $CODE -ne 0; then
   echo "gradle $TASK error"; exit $((100+i))
@@ -18,12 +19,14 @@ for ((i=0; i<SIZE; i++)); do
 done
 
 ENVIRONMENT=repository/buildSrc/src/main/resources/json/unit_test.json
-gradle -p repository "$(jq -Mcer ".UNIT_TEST.task" $ENVIRONMENT)"; CODE=$?
+TYPE="UNIT_TEST"
+gradle -p repository "$(jq -Mcer ".${TYPE}.task" $ENVIRONMENT)"; CODE=$?
 if test $CODE -ne 0; then
  echo "Unit test failed!"; exit $((200+i))
 else
- gradle -p repository "$(jq -Mcer ".UNIT_TEST.coverage.task" $ENVIRONMENT)" || exit 1 # todo
- gradle -p repository "$(jq -Mcer ".UNIT_TEST.coverage.verification.task" $ENVIRONMENT)"; CODE=$?
+ TYPE="TEST_COVERAGE"
+ gradle -p repository "$(jq -Mcer ".${TYPE}.task" $ENVIRONMENT)" || exit 1 # todo
+ gradle -p repository "$(jq -Mcer ".${TYPE}.verification.task" $ENVIRONMENT)"; CODE=$?
  if test $CODE -ne 0; then
   echo "Unit test coverage verification failed!"; exit $((200+i))
  fi
