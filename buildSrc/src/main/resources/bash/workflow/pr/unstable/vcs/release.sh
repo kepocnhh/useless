064d4a0d32bcf1273eeb82b762c9381267d4a498
Stanley Wintergreen <kepocnhh@gmail.com>
2022-06-08 23:30:56 +0300

    GitHub release upload artifact

--- buildSrc/src/main/resources/bash/workflow/pr/unstable/vcs/release.sh
+++ buildSrc/src/main/resources/bash/workflow/pr/unstable/vcs/release.sh
@@ -14,6 +14,7 @@ TAG="${VERSION_NAME}-UNSTABLE"
 /bin/bash $SCRIPTS/workflow/pr/unstable/assemble/project/artifact.sh || exit 13
 /bin/bash $SCRIPTS/vcs/push.sh || exit 14
 /bin/bash $SCRIPTS/assemble/vcs/commit.sh || exit 15
+
 GIT_COMMIT_SHA="$(jq -Mcer ".sha|$REQUIRE_FILLED_STRING" assemble/vcs/commit.json)" || exit 1 # todo
 BODY="$(echo "{}" | jq -Mc ".name=\"$TAG\"")"
 BODY="$(echo "$BODY" | jq -Mc ".tag_name=\"$TAG\"")"
@@ -23,6 +24,13 @@ BODY="$(echo "$BODY" | jq -Mc ".draft=false")"
 BODY="$(echo "$BODY" | jq -Mc ".prerelease=true")"
 mkdir -p assemble/github
 /bin/bash $SCRIPTS/github/release.sh "$BODY" || exit 16
+
+ARTIFACT_NAME="${REPOSITORY_NAME}-${TAG}.jar"
+ARTIFACT="$(echo "{}" | jq -Mc ".name=\"$ARTIFACT_NAME\"")"
+ARTIFACT="$(echo "$ARTIFACT" | jq -Mc ".label=\"$ARTIFACT_NAME\"")"
+ARTIFACT="$(echo "$ARTIFACT" | jq -Mc ".path=\"assemble/project/artifact/$ARTIFACT_NAME\"")"
+ARTIFACTS="$(echo "[]" | jq -Mc ".+=[$ARTIFACT]")"
+/bin/bash $SCRIPTS/github/release/upload/artifact.sh "$ARTIFACTS" || exit 17
 
 exit 2 # todo
 
