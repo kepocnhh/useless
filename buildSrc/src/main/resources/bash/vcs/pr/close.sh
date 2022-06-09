80ca5a35bfbb62fbd38994179633791578500ff5
Stanley Wintergreen <kepocnhh@gmail.com>
2022-06-09 17:05:40 +0300

    fix close pull request

--- buildSrc/src/main/resources/bash/vcs/pr/close.sh
+++ buildSrc/src/main/resources/bash/vcs/pr/close.sh
@@ -12,7 +12,7 @@ CODE=$(curl -w %{http_code} -o /dev/null -X PATCH \
  "$VCS_DOMAIN/repos/$REPOSITORY_OWNER/$REPOSITORY_NAME/pulls/$PR_NUMBER" \
  -H "Authorization: token $VCS_PAT" \
  -d "$BODY")
-if test $CODE -ne 201; then
+if test $CODE -ne 200; then
  echo "Close pull request #$PR_NUMBER error!"
  echo "Request error with response code $CODE!"
  exit 31
