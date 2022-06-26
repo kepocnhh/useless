e72fac83e0f7f627122a37fd1b6b8c22e6b938a7
Stanley Wintergreen <kepocnhh@gmail.com>
2022-06-26 20:00:07 +0300

    assemble vcs repository

--- buildSrc/src/main/resources/bash/assemble/vcs/repository.sh
+++ buildSrc/src/main/resources/bash/assemble/vcs/repository.sh
@@ -2,19 +2,22 @@
 
 echo "Assemble VCS repository..."
 
-for it in REPOSITORY_OWNER REPOSITORY_NAME VCS_DOMAIN VCS_PAT; do
- if test -z "${!it}"; then echo "$it is empty!"; exit 11; fi; done
+SCRIPTS=repository/buildSrc/src/main/resources/bash
+
+. $SCRIPTS/util/require REPOSITORY_OWNER REPOSITORY_NAME VCS_DOMAIN
 
 CODE=0
 CODE=$(curl -w %{http_code} -o assemble/vcs/repository.json \
- "$VCS_DOMAIN/repos/$REPOSITORY_OWNER/$REPOSITORY_NAME" \
- -H "Authorization: token $VCS_PAT")
+ "$VCS_DOMAIN/repos/$REPOSITORY_OWNER/$REPOSITORY_NAME")
 if test $CODE -ne 200; then
  echo "Get repository ${GITLAB_PROJECT} error!"
  echo "Request error with response code $CODE!"
  exit 21
 fi
 
-echo "The repository $(jq -r .html_url assemble/vcs/repository.json) is ready."
+REPOSITORY_HTML_URL=$($SCRIPTS/util/jqx -sfs assemble/vcs/repository.json .html_url) \
+ || . $SCRIPTS/util/throw $? "$(cat /tmp/jqx.o)"
+
+echo "The repository $REPOSITORY_HTML_URL is ready."
 
 exit 0
