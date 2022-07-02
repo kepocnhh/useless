643da013ade07d0eb38124a786907d919fa03bec
Stanley Wintergreen <kepocnhh@gmail.com>
2022-07-02 23:08:54 +0300

    fix iss/10

--- buildSrc/src/main/resources/bash/workflow/pr/staging/release/note/markdown.sh
+++ buildSrc/src/main/resources/bash/workflow/pr/staging/release/note/markdown.sh
@@ -12,25 +12,26 @@ SCRIPTS=repository/buildSrc/src/main/resources/bash
 
 . $SCRIPTS/util/require TAG
 
+RELEASE_NOTE="Release note \`$TAG\`"
 SIZE=$(jq -e "length" assemble/github/fixed.json) || exit 1 # todo
 if test $SIZE -eq 0; then
- echo "Release note $TAG
- - not a single issue has been resolved" > assemble/github/release_note.md
- exit 0
-fi
-RELEASE_NOTE="Release note $TAG
-Fixed:"
-FILE="assemble/github/fixed.json"
-for ((i=0; i<SIZE; i++)); do
- ISSUE_NUMBER=$($SCRIPTS/util/jqx -si "$FILE" ".[$i].number") \
-  || . $SCRIPTS/util/throw $? "$(cat /tmp/jqx.o)"
- ISSUE_TITLE=$($SCRIPTS/util/jqx -sfs "$FILE" ".[$i].title") \
-  || . $SCRIPTS/util/throw $? "$(cat /tmp/jqx.o)"
- ISSUE_HTML_URL=$($SCRIPTS/util/jqx -sfs "$FILE" ".[$i].html_url") \
-  || . $SCRIPTS/util/throw $? "$(cat /tmp/jqx.o)"
  RELEASE_NOTE="$RELEASE_NOTE
- - [#$ISSUE_NUMBER]($ISSUE_HTML_URL) $ISSUE_TITLE"
-done
+ - not a single issue has been resolved"
+else
+ RELEASE_NOTE="$RELEASE_NOTE
+Fixed:"
+ FILE="assemble/github/fixed.json"
+ for ((i=0; i<SIZE; i++)); do
+  ISSUE_NUMBER=$($SCRIPTS/util/jqx -si "$FILE" ".[$i].number") \
+   || . $SCRIPTS/util/throw $? "$(cat /tmp/jqx.o)"
+  ISSUE_TITLE=$($SCRIPTS/util/jqx -sfs "$FILE" ".[$i].title") \
+   || . $SCRIPTS/util/throw $? "$(cat /tmp/jqx.o)"
+  ISSUE_HTML_URL=$($SCRIPTS/util/jqx -sfs "$FILE" ".[$i].html_url") \
+   || . $SCRIPTS/util/throw $? "$(cat /tmp/jqx.o)"
+  RELEASE_NOTE="$RELEASE_NOTE
+  - [#$ISSUE_NUMBER]($ISSUE_HTML_URL) $ISSUE_TITLE"
+ done
+fi
 
 echo "$RELEASE_NOTE" > assemble/github/release_note.md
 
