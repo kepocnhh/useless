dd1c327082f7cda60d3ce28a41580c1fb707e37e
Stanley Wintergreen <kepocnhh@gmail.com>
2022-07-04 02:32:57 +0300

    fix documentation

--- lib/src/main/kotlin/org/kepocnhh/useless/DoubleUtil.kt
+++ lib/src/main/kotlin/org/kepocnhh/useless/DoubleUtil.kt
@@ -1,10 +1,18 @@
 package org.kepocnhh.useless
 
+/**
+ * If epsilon `0.0001` then `0.11113` equal `0.11111`.
+ * If epsilon `0.00001` then `0.11113` more than `0.11111`.
+ */
 fun Double.isMoreThen(that: Double, epsilon: Double): Boolean {
     check(epsilon < 1)
     return this - that > epsilon
 }
 
+/**
+ * If epsilon `0.0001` then `0.11111` equal `0.11113`.
+ * If epsilon `0.00001` then `0.11111` less than `0.11113`.
+ */
 fun Double.isLessThen(that: Double, epsilon: Double): Boolean {
     check(epsilon < 1)
     return that - this > epsilon
