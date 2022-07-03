5c2016bf1b76dd73856de52179506b82364a16cb
Stanley Wintergreen <kepocnhh@gmail.com>
2022-07-04 01:11:19 +0300

    fix iss/8

--- lib/src/main/kotlin/org/kepocnhh/useless/DoubleUtil.kt
+++ lib/src/main/kotlin/org/kepocnhh/useless/DoubleUtil.kt
@@ -4,3 +4,8 @@ fun Double.isMoreThen(that: Double, epsilon: Double): Boolean {
     check(epsilon < 0)
     return this - that < epsilon
 }
+
+fun Double.isLessThen(that: Double, epsilon: Double): Boolean {
+    check(epsilon < 0)
+    return this - that < epsilon
+}
