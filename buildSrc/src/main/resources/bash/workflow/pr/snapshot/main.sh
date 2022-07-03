d3f4a2342e74f00756af90670416b48ac132f55d
Stanley Wintergreen <kepocnhh@gmail.com>
2022-07-03 13:37:24 +0300

    workflow pr staging

---
+++ buildSrc/src/main/resources/bash/workflow/pr/snapshot/main.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Workflow pull request snapshot start..."
+
+SCRIPTS=repository/buildSrc/src/main/resources/bash
+
+exit 1 # todo
+
+echo "Workflow pull request snapshot finish."
+
+exit 0
