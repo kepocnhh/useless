495033c0ec28e79fba14c54886c5d4bfdb6a986a
Stanley Wintergreen <kepocnhh@gmail.com>
2022-07-04 01:47:52 +0300

    fix code quality

--- lib/src/test/kotlin/org/kepocnhh/useless/DoubleUtilTest.kt
+++ lib/src/test/kotlin/org/kepocnhh/useless/DoubleUtilTest.kt
@@ -1,8 +1,8 @@
 package org.kepocnhh.useless
 
-import org.junit.Assert.assertTrue
 import org.junit.Assert.assertFalse
-import org.junit.Assert.fail
+import org.junit.Assert.assertThrows
+import org.junit.Assert.assertTrue
 import org.junit.Test
 
 internal class DoubleUtilTest {
@@ -12,12 +12,9 @@ internal class DoubleUtilTest {
         val b = 0.11111
         assertFalse("equals", a.isMoreThen(b, epsilon = 0.0001))
         assertTrue("more", a.isMoreThen(b, epsilon = 0.00001))
-        try {
+        assertThrows(IllegalStateException::class.java) {
             a.isMoreThen(b, epsilon = 1.0)
-        } catch (e: Throwable) {
-            return
         }
-        fail()
     }
 
     @Test
@@ -26,11 +23,8 @@ internal class DoubleUtilTest {
         val b = 0.11113
         assertFalse("equals", a.isLessThen(b, epsilon = 0.0001))
         assertTrue("less", a.isLessThen(b, epsilon = 0.00001))
-        try {
+        assertThrows(IllegalStateException::class.java) {
             a.isLessThen(b, epsilon = 1.0)
-        } catch (e: Throwable) {
-            return
         }
-        fail()
     }
 }
