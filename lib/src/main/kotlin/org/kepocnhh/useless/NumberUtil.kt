e7e72ebea952b4d6dd81e0908e614677e091e3c6
Stanley Wintergreen <kepocnhh@gmail.com>
2022-07-03 17:20:15 +0300

    fix documentation

--- lib/src/main/kotlin/org/kepocnhh/useless/NumberUtil.kt
+++ lib/src/main/kotlin/org/kepocnhh/useless/NumberUtil.kt
@@ -1,26 +1,44 @@
 package org.kepocnhh.useless
 
+/**
+ * Test documentation class.
+ */
 object NumberUtil {
     private const val ZERO = 0
     private const val ONE = 1
     private const val TWO = 2
 
+    /**
+     * Test documentation method.
+     */
     fun getZero(): Int {
         return ZERO
     }
 
+    /**
+     * If you suddenly need the number one.
+     */
     fun getOne(): Int {
         return ONE
     }
 
+    /**
+     * Number 2.
+     */
     fun getTwo(): Int {
         return TWO
     }
 
+    /**
+     * Check is zero.
+     */
     fun isZero(value: Int): Boolean {
         return value == ZERO
     }
 
+    /**
+     * one?
+     */
     fun isOne(value: Int): Boolean {
         return value == ONE
     }
