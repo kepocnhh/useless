f41e7ff3a2a066f21ebbe94f0a6adfe5031dd838
Stanley Wintergreen <kepocnhh@gmail.com>
2022-09-05 19:50:15 +0300

    kotlin:1.6.21

--- buildSrc/src/main/kotlin/Version.kt
+++ buildSrc/src/main/kotlin/Version.kt
@@ -4,7 +4,7 @@ object Version {
     const val jacoco = "0.8.8"
     const val jvmTarget = "1.8"
     const val junit = "4.13.2"
-    const val kotlin = "1.5.31"
+    const val kotlin = "1.6.21"
     const val ktlint = "0.45.2"
     const val name = "0.6"
 }
